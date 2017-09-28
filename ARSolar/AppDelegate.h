3915dca9f94f99ad74b05ba279180adf0efc0c37
jasonliu <liuhengmao@163.com>
2017-09-28 04:36:32 -0500

    Add files via upload

---
+++ ARSolar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARSolar
+//
+//  Created by wendao on 2017/7/7.
+//  Copyright © 2017年 wendao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
